86de5bf07a9ed37dff8b6be098628cb4af74ea49
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-03-01 15:10:43 -0500

    main: pull argument parsing out + add enum
    Unnecessary, just practicing rust enum stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use futures::executor::block_on;
+use std::error::Error;
+use std::fmt;
 use tonic_lnd::{Client, ConnectError};
 
 #[tokio::main]
 async fn main() {
-    let mut client = get_lnd_client().expect("failed to connect");
+    let args = match parse_args() {
+        Ok(args) => args,
+        Err(args) => panic!("Bad arguments: {}", args),
+    };
+
+    let mut client = get_lnd_client(args).expect("failed to connect");
 
     let info = client
         .lightning()
@@ -16,18 +23,68 @@ async fn main() {
     println!("{:#?}", info);
 }
 
-fn get_lnd_client() -> Result<Client, ConnectError> {
-    let mut args = std::env::args_os();
-    args.next().expect("not even zeroth arg given");
-    let address = args
-        .next()
-        .expect("missing arguments: address, cert file, macaroon file");
-    let cert_file = args
-        .next()
-        .expect("missing arguments: cert file, macaroon file");
-    let macaroon_file = args.next().expect("missing argument: macaroon file");
-    let address = address.into_string().expect("address is not UTF-8");
+fn get_lnd_client(cfg: LndCfg) -> Result<Client, ConnectError> {
+    block_on(tonic_lnd::connect(cfg.address, cfg.cert, cfg.macaroon))
+}
 
-    // Connecting to LND requires only address, cert file, and macaroon file
-    block_on(tonic_lnd::connect(address, cert_file, macaroon_file))
+#[derive(Debug)]
+enum ArgsError {
+    NoArgs,
+    AddressRequired,
+    CertRequired,
+    MacaroonRequired,
+}
+
+impl Error for ArgsError {}
+
+impl fmt::Display for ArgsError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ArgsError::NoArgs => write!(f, "No command line arguments provided."),
+            ArgsError::AddressRequired => write!(f, "LND's RPC server address is required."),
+            ArgsError::CertRequired => write!(f, "Path to LND's tls certificate is required."),
+            ArgsError::MacaroonRequired => write!(f, "Path to LND's macaroon is required."),
+        }
+    }
+}
+
+struct LndCfg {
+    address: String,
+    cert: String,
+    macaroon: String,
+}
+
+impl LndCfg {
+    fn new(address: String, cert: String, macaroon: String) -> LndCfg {
+        LndCfg {
+            address: address,
+            cert: cert,
+            macaroon: macaroon,
+        }
+    }
+}
+
+fn parse_args() -> Result<LndCfg, ArgsError> {
+    let mut args = std::env::args_os();
+    match args.next() {
+        None => return Err(ArgsError::NoArgs),
+        _ => {}
+    };
+
+    let address = match args.next() {
+        Some(arg) => arg.into_string().expect("address is not UTF-8"),
+        None => return Err(ArgsError::AddressRequired),
+    };
+
+    let cert_file = match args.next() {
+        Some(arg) => arg.into_string().expect("cert is not UTF-8"),
+        None => return Err(ArgsError::CertRequired),
+    };
+
+    let macaroon_file = match args.next() {
+        Some(arg) => arg.into_string().expect("macaroon is not UTF-8"),
+        None => return Err(ArgsError::MacaroonRequired),
+    };
+
+    Ok(LndCfg::new(address, cert_file, macaroon_file))
 }
