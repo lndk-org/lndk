53ec4cd8f7ec56f31ae7684eac210e5d50d9d8c9
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-03-07 13:41:43 -0500

    main: implement NodeSigner using LND signerrpc

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use bitcoin::bech32::u5;
+use bitcoin::secp256k1::ecdh::SharedSecret;
+use bitcoin::secp256k1::ecdsa::{RecoverableSignature, Signature};
+use bitcoin::secp256k1::{self, PublicKey, Scalar, Secp256k1};
 use futures::executor::block_on;
+use lightning::chain::keysinterface::{KeyMaterial, NodeSigner, Recipient};
+use lightning::ln::msgs::UnsignedGossipMessage;
+use std::cell::RefCell;
 use std::error::Error;
 use std::fmt;
+use std::str::FromStr;
 use tonic_lnd::{Client, ConnectError};
 
 #[tokio::main]
@@ -18,9 +26,103 @@ async fn main() {
         .await
         .expect("failed to get info");
 
-    // We only print it here, note that in real-life code you may want to call `.into_inner()` on
-    // the response to get the message.
-    println!("{:#?}", info);
+    let pubkey = PublicKey::from_str(&info.into_inner().identity_pubkey).unwrap();
+    println!("Starting lndk for node: {}", pubkey);
+
+    let _node_signer = LndNodeSigner::new(pubkey, client.signer());
+}
+
+struct LndNodeSigner<'a> {
+    pubkey: PublicKey,
+    secp_ctx: Secp256k1<secp256k1::All>,
+    signer: RefCell<&'a mut tonic_lnd::SignerClient>,
+}
+
+impl<'a> LndNodeSigner<'a> {
+    fn new(pubkey: PublicKey, signer: &'a mut tonic_lnd::SignerClient) -> Self {
+        LndNodeSigner {
+            pubkey: pubkey,
+            secp_ctx: Secp256k1::new(),
+            signer: RefCell::new(signer),
+        }
+    }
+}
+
+impl<'a> NodeSigner for LndNodeSigner<'a> {
+    /// Get node id based on the provided [`Recipient`].
+    ///
+    /// This method must return the same value each time it is called with a given [`Recipient`]
+    /// parameter.
+    ///
+    /// Errors if the [`Recipient`] variant is not supported by the implementation.
+    fn get_node_id(&self, recipient: Recipient) -> Result<PublicKey, ()> {
+        match recipient {
+            Recipient::Node => return Ok(self.pubkey),
+            Recipient::PhantomNode => return Err(()),
+        }
+    }
+
+    /// Gets the ECDH shared secret of our node secret and `other_key`, multiplying by `tweak` if
+    /// one is provided. Note that this tweak can be applied to `other_key` instead of our node
+    /// secret, though this is less efficient.
+    ///
+    /// Errors if the [`Recipient`] variant is not supported by the implementation.
+    fn ecdh(
+        &self,
+        recipient: Recipient,
+        other_key: &PublicKey,
+        tweak: Option<&Scalar>,
+    ) -> Result<SharedSecret, ()> {
+        match recipient {
+            Recipient::Node => {}
+            Recipient::PhantomNode => return Err(()),
+        }
+
+        // Clone other_key so that we can tweak it (if a tweak is required). We choose to tweak the
+        // `other_key` because LND's API accept a tweak parameter (so we can't tweak our secret).
+        let mut tweaked_key = other_key.clone();
+        if let Some(tweak) = tweak {
+            tweaked_key = tweaked_key
+                .mul_tweak(&self.secp_ctx, tweak)
+                .map_err(|_| ())?;
+        };
+
+        let shared_secret = match block_on(self.signer.borrow_mut().derive_shared_key(
+            tonic_lnd::signrpc::SharedKeyRequest {
+                ephemeral_pubkey: tweaked_key.serialize().into_iter().collect::<Vec<u8>>(),
+                key_loc: None,
+                key_desc: None,
+            },
+        )) {
+            Ok(shared_key_resp) => shared_key_resp.into_inner().shared_key,
+            Err(_) => return Err(()),
+        };
+
+        match SharedSecret::from_slice(&shared_secret) {
+            Ok(secret) => Ok(secret),
+            Err(_) => Err(()),
+        }
+    }
+
+    /// Unimplemented: not required for onion messaging.
+    fn get_inbound_payment_key_material(&self) -> KeyMaterial {
+        unimplemented!();
+    }
+
+    /// Unimplemented: not required for onion messaging.
+    fn sign_invoice(
+        &self,
+        _hrp_bytes: &[u8],
+        _invoice_data: &[u5],
+        _recipient: Recipient,
+    ) -> Result<RecoverableSignature, ()> {
+        unimplemented!();
+    }
+
+    /// Unimplemented: not required for onion messaging.
+    fn sign_gossip_message(&self, _msg: UnsignedGossipMessage) -> Result<Signature, ()> {
+        unimplemented!();
+    }
 }
 
 fn get_lnd_client(cfg: LndCfg) -> Result<Client, ConnectError> {
