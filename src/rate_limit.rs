61170bfc357e0f7b7bbe6417126a4a7f34691339
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-05-05 11:30:34 -0400

    multi: make internal peers map private and surface via method

--- src/rate_limit.rs
+++ src/rate_limit.rs
@@ -3,19 +3,23 @@ use std::collections::HashMap;
 
 // CurrentPeers keeps up-to-date with all of the peers we're connected and disconnected to using a map.
 pub(crate) struct CurrentPeers {
-    pub(crate) map: HashMap<PublicKey, bool>,
+    peer_map: HashMap<PublicKey, bool>,
 }
 
 impl CurrentPeers {
     pub(crate) fn new(peers: HashMap<PublicKey, bool>) -> CurrentPeers {
-        CurrentPeers { map: peers }
+        CurrentPeers { peer_map: peers }
     }
 
     pub(crate) fn peer_connected(&mut self, peer_key: PublicKey, onion_support: bool) {
-        self.map.insert(peer_key, onion_support);
+        self.peer_map.insert(peer_key, onion_support);
     }
 
     pub(crate) fn peer_disconnected(&mut self, peer_key: PublicKey) {
-        self.map.remove(&peer_key);
+        self.peer_map.remove(&peer_key);
+    }
+
+    pub(crate) fn peers(&self) -> Vec<PublicKey> {
+        self.peer_map.keys().cloned().collect::<Vec<PublicKey>>()
     }
 }
