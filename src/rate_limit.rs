0ce8713e1504b937135c36c22b764e323fb799de
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-05-09 15:28:09 -0400

    multi: add rate limiter trait implemented by TokenLimiter

--- src/rate_limit.rs
+++ src/rate_limit.rs
@@ -1,6 +1,13 @@
 use bitcoin::secp256k1::PublicKey;
 use std::collections::HashMap;
 
+/// RateLimiter provides peer tracking and rate limiting for lightning peers. 
+pub(crate) trait RateLimiter {
+    fn peer_connected(&mut self, peer_key: PublicKey);
+    fn peer_disconnected(&mut self, peer_key: PublicKey);
+    fn peers(&self) -> Vec<PublicKey>;
+}
+
 // TokenLimiter keeps a map of currently online peers.
 pub(crate) struct TokenLimiter {
     peer_map: HashMap<PublicKey, bool>,
@@ -10,16 +17,18 @@ impl TokenLimiter {
     pub(crate) fn new(peers: HashMap<PublicKey, bool>) -> TokenLimiter {
         TokenLimiter { peer_map: peers }
     }
+}
 
-    pub(crate) fn peer_connected(&mut self, peer_key: PublicKey) {
+impl RateLimiter for TokenLimiter {
+    fn peer_connected(&mut self, peer_key: PublicKey) {
         self.peer_map.insert(peer_key, true);
     }
 
-    pub(crate) fn peer_disconnected(&mut self, peer_key: PublicKey) {
+    fn peer_disconnected(&mut self, peer_key: PublicKey) {
         self.peer_map.remove(&peer_key);
     }
 
-    pub(crate) fn peers(&self) -> Vec<PublicKey> {
+    fn peers(&self) -> Vec<PublicKey> {
         self.peer_map.keys().cloned().collect::<Vec<PublicKey>>()
     }
 }
