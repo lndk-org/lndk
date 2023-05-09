561afe18be4897ccb826681524a3f0f662c1ef5c
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-05-09 16:51:00 -0400

    multi: add clock module for handling of time, implement with tokio

---
+++ src/clock.rs
@@ -0,0 +1,20 @@
+use tokio::time::Instant;
+
+pub trait Clock {
+    fn now(&self) -> Instant;
+}
+
+/// TokioClock provides an implementation of the Clock trait using Tokio's time module.
+pub struct TokioClock;
+
+impl TokioClock {
+    pub fn new() -> Self {
+        TokioClock
+    }
+}
+
+impl Clock for TokioClock {
+    fn now(&self) -> Instant {
+        Instant::now()
+    }
+}
