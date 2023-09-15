a61b192a16760c69505e69622d9ea5c2f431bbd0
Orbital <orbitalturtle@protonmail.com>
2023-09-15 03:29:13 -0500

    tests: organize needed bitcoind data in a struct

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -3,7 +3,7 @@ mod common;
 #[tokio::test(flavor = "multi_thread")]
 async fn test_ldk_send_onion_message() {
     let test_name = "send_onion_message";
-    let (_bitcoind, _bitcoin_dir, ldk1, ldk2) = common::setup_test_infrastructure(test_name).await;
+    let (_bitcoind, ldk1, ldk2) = common::setup_test_infrastructure(test_name).await;
     let (node_id_2, node_addr_2) = ldk2.get_node_info();
     ldk1.connect_to_peer(node_id_2, node_addr_2).await.unwrap();
 
