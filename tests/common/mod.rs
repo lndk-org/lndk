b9fdec5f9ef389c3cdfcbc9eeda085d2e4680559
Orbital <orbitalturtle@protonmail.com>
2024-01-11 21:07:47 -0600

    itests: add lnd new_address api call

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -15,7 +15,7 @@ use std::thread;
 use std::{env, fs};
 use tempfile::{tempdir, Builder, TempDir};
 use tokio::time::{sleep, timeout, Duration};
-use tonic_lnd::lnrpc::GetInfoRequest;
+use tonic_lnd::lnrpc::{AddressType, GetInfoRequest};
 use tonic_lnd::Client;
 
 const LNDK_TESTS_FOLDER: &str = "lndk-tests";
@@ -396,5 +396,30 @@ impl LndNode {
             }
             sleep(Duration::from_secs(2)).await;
         }
+    }
+
+    // Create an on-chain bitcoin address to fund our LND node.
+    #[allow(dead_code)]
+    pub async fn new_address(&mut self) -> tonic_lnd::lnrpc::NewAddressResponse {
+        let addr_req = tonic_lnd::lnrpc::NewAddressRequest {
+            r#type: AddressType::TaprootPubkey.into(),
+            ..Default::default()
+        };
+
+        let resp = if let Some(client) = self.client.clone() {
+            let make_request = || async {
+                client
+                    .clone()
+                    .lightning()
+                    .new_address(addr_req.clone())
+                    .await
+            };
+            let resp = test_utils::retry_async(make_request, String::from("new_address"));
+            resp.await.unwrap()
+        } else {
+            panic!("No client")
+        };
+
+        resp
     }
 }
