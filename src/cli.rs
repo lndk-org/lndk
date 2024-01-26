06470b2569c7c60cfcbec919d9d0117e66099555
Orbital <orbitalturtle@protonmail.com>
2024-01-26 16:31:46 -0600

    cli: add global arguments for connecting to lnd

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,54 @@
 use clap::{Parser, Subcommand};
 use lndk::lndk_offers::decode;
+use std::ffi::OsString;
+
+fn get_cert_path_default() -> OsString {
+    home::home_dir()
+        .unwrap()
+        .as_path()
+        .join(".lnd")
+        .join("tls.cert")
+        .into_os_string()
+}
+
+fn get_macaroon_path_default() -> OsString {
+    home::home_dir()
+        .unwrap()
+        .as_path()
+        .join(".lnd/data/chain/bitcoin/regtest/admin.macaroon")
+        .into_os_string()
+}
 
 /// A cli for interacting with lndk.
 #[derive(Debug, Parser)]
 #[command(name = "lndk-cli")]
 #[command(about = "A cli for interacting with lndk", long_about = None)]
 struct Cli {
+    /// Global variables
+    #[arg(
+        short,
+        long,
+        global = true,
+        required = false,
+        default_value = "regtest"
+    )]
+    network: String,
+
+    #[arg(short, long, global = true, required = false, default_value = get_cert_path_default())]
+    tls_cert: String,
+
+    #[arg(short, long, global = true, required = false, default_value = get_macaroon_path_default())]
+    macaroon: String,
+
+    #[arg(
+        short,
+        long,
+        global = true,
+        required = false,
+        default_value = "https://localhost:10009"
+    )]
+    address: String,
+
     #[command(subcommand)]
     command: Commands,
 }
