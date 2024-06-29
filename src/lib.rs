c492497efa17102c0e9c4b2666807c07b8acb06f
Maurice Poirrier Chuden <mauricepoirrier@gmail.com>
2024-06-29 16:06:10 -0400

    server: expose get info and pay offer
    this commit expose both endpoints. We use a hex serialization for invoices. We should move to use a readable from ldk and then hex serialize it in the future

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ use bitcoin::secp256k1::{Error as Secp256k1Error, PublicKey, Secp256k1};
 use home::home_dir;
 use lightning::blinded_path::BlindedPath;
 use lightning::ln::inbound_payment::ExpandedKey;
+use lightning::ln::msgs::DecodeError;
 use lightning::ln::peer_handler::IgnoringMessageHandler;
 use lightning::offers::invoice::Bolt12Invoice;
 use lightning::offers::invoice_error::InvoiceError;
@@ -463,6 +464,23 @@ impl OffersMessageHandler for OfferHandler {
 
     fn release_pending_messages(&self) -> Vec<PendingOnionMessage<OffersMessage>> {
         core::mem::take(&mut self.pending_messages.lock().unwrap())
+    }
+}
+
+pub struct Bolt12InvoiceString(pub String);
+
+impl TryFrom<Bolt12InvoiceString> for Bolt12Invoice {
+    type Error = DecodeError;
+
+    fn try_from(s: Bolt12InvoiceString) -> Result<Self, Self::Error> {
+        let bytes: Vec<u8> = hex::decode(s.0).unwrap();
+        Self::try_from(bytes).map_err(|_| DecodeError::InvalidValue)
+    }
+}
+
+impl From<String> for Bolt12InvoiceString {
+    fn from(s: String) -> Self {
+        Bolt12InvoiceString(s)
     }
 }
 
