a2b42b5a7985ef780dbaadf3a8ad2bca8f96b70d
Maurice Poirrier Chuden <mauricepoirrier@gmail.com>
2024-11-08 12:42:32 -0300

    server: default lndk conf in lndk dir
    LNDK is already using a datadir to store TLS certificates.
    In this PR we default LNDK config and data to ~/.lndk.
    Datadir is moved inside the default dir ~/.lndk/data. Then,
    we expect lndk.conf file to be iether in root of the project,
    or the default LNDK config folder or --config flag.
    Precedence is flag > root > default dir.

--- src/lib.rs
+++ src/lib.rs
@@ -62,8 +62,10 @@ pub fn init_logger(config: LogConfig) {
 pub const DEFAULT_SERVER_HOST: &str = "127.0.0.1";
 pub const DEFAULT_SERVER_PORT: u16 = 7000;
 pub const LDK_LOGGER_NAME: &str = "ldk";
-pub const DEFAULT_DATA_DIR: &str = ".lndk";
+pub const DEFAULT_DATA_DIR: &str = "data";
+pub const DEFAULT_LNDK_DIR: &str = ".lndk";
 pub const DEFAULT_LOG_FILE: &str = "lndk.log";
+pub const DEFAULT_CONFIG_FILE_NAME: &str = "lndk.conf";
 
 pub const TLS_CERT_FILENAME: &str = "tls-cert.pem";
 pub const TLS_KEY_FILENAME: &str = "tls-key.pem";
